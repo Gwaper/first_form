19592b6568aa0aec80ce42f585ed364248bc3d82
olivier Tristan <tristanolivier.work@gmail.com>
2019-04-15 17:00:57 +0200

    so french

--- form/src/FormEmployee.js
+++ form/src/FormEmployee.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
 import './FormEmployee.css'
 
+
+
 class FormEmployee extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            lastname: '',
-            firstname: '',
-            email: '',
+            name: '',
+            poster: '',
+            comment: '',
         }
+        this.onChange = this.onChange.bind(this);
+        this.submitForm = this.submitForm.bind(this);
+      
     }
     onChange(e) {
         this.setState({
@@ -18,57 +23,81 @@ class FormEmployee extends Component {
 
     submitForm(e) {
         e.preventDefault();
+        const config = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(this.state),
+        };
+        const url = "http://campus-bordeaux.ovh:3001/api/quests/movies/";
+    
+        fetch(url, config)
+        .then(res => res.json())
+         .then(res => {
+            if (res.error) {
+                alert(res.error);
+            } else {
+                alert(`Putain de chaleur ${res}!`);
+            }
+        }).catch(e => {
+            console.error(e);
+            alert('hahah vous n\'avez pas dit le mot magique ');
+        });
+    
     }
 
-    render() {
-        return (
-            <div className="FormEmployee">
-                <h1>Saisi d'un employé</h1>
+    
 
-                <form onSubmit={this.submitForm}>
-                    <fieldset>
-                        <legend>Informations</legend>
-                        <div className="form-data">
-                            <label htmlFor="lastname">Nom</label>
-                            <input
-                                type="text"
-                                id="lastname"
-                                name="lastname"
-                                onChange={this.onChange}
-                                value={this.state.lastname}
-                            />
-                        </div>
+render() {
+    return (
+        <div className="FormEmployee">
+            <h1>Titre de Film</h1>
 
-                        <div className="form-data">
-                            <label htmlFor="firstname">Prénom</label>
-                            <input
-                                type="text"
-                                id="firstname"
-                                name="firstname"
-                                onChange={this.onChange}
-                                value={this.state.firstname}
-                            />
-                        </div>
+            <form onSubmit={this.submitForm}>
+                <fieldset>
+                    <legend>Informations</legend>
+                    <div className="form-data">
+                        <label htmlFor="name">Titre</label>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            onChange={this.onChange}
+                            value={this.state.name}
+                        />
+                    </div>
 
-                        <div className="form-data">
-                            <label htmlFor="email">E-mail</label>
-                            <input
-                                type="email"
-                                id="email"
-                                name="email"
-                                onChange={this.onChange}
-                                value={this.state.email}
-                            />
-                        </div>
-                        <hr />
-                        <div className="form-data">
-                            <input type="submit" value="Envoyer" />
-                        </div>
-                    </fieldset>
-                </form>
-            </div>
-        );
-    }
+                    <div className="form-data">
+                        <label htmlFor="poster">Poster</label>
+                        <input
+                            type="text"
+                            id="poster"
+                            name="poster"
+                            onChange={this.onChange}
+                            value={this.state.url}
+                        />
+                    </div>
+
+                    <div className="form-data">
+                        <label htmlFor="comment">Comment</label>
+                        <textarea
+                            type="comment"
+                            id="comment"
+                            name="comment"
+                            onChange={this.onChange}
+                            value={this.state.comment}
+                        />
+                    </div>
+                    <hr />
+                    <div className="form-data">
+                        <input type="submit" value="Envoyer" />
+                    </div>
+                </fieldset>
+            </form>
+        </div>
+    );
+}
 }
 
 export default FormEmployee;
